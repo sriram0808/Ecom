89ae953d8d32d37305f7c126db95b5e80f1e21af
sriram0808 <sriram08082000@gmail.com>
2024-05-20 12:37:20 +0530

    bysri

--- mobile/src/Components/Editprofile.jsx
+++ mobile/src/Components/Editprofile.jsx
@@ -8,7 +8,7 @@ let Editprofile = () => {
       <div className="container mt-5 p-5" style={{  borderRadius: "5px", backgroundColor: "rgb(248,248,248)" }}>
         <div className="row mb-3 align-items-center">
           <div className="col">
-            <label htmlFor="" className="mb-2">Name</label>
+            <label htmlFor="" className="mb-2  editLabel">Name <span className="text-danger">*</span></label>
             <input type="text" className="form-control" placeholder="Sri Ram" />
           </div>
           <div className="col-auto">
@@ -17,7 +17,7 @@ let Editprofile = () => {
         </div>
         <div className="row mb-3 align-items-center">
           <div className="col">
-            <label htmlFor="" className="mb-2">Email</label>
+            <label htmlFor="" className="mb-2 editLabel">Email <span className="text-danger">*</span></label>
             <input type="email" className="form-control" placeholder="abc@gmail.com" />
           </div>
           <div className="col-auto">
@@ -26,25 +26,25 @@ let Editprofile = () => {
         </div>
         <div className="row mb-3 align-items-center">
           <div className="col">
-            <label htmlFor="" className="mb-2">Mobile Number</label>
+            <label htmlFor="" className="mb-2 editLabel">Mobile Number <span className="text-danger">*</span></label>
             <input type="text" className="form-control" placeholder="***********98" />
           </div>
           <div className="col-auto">
-            <button className="btn btn-primary my-4 editButton">Edit</button>
+            <button className="btn btn-primary my-4 editButton">Edit <span className="text-danger">*</span></button>
           </div>
         </div>
         <div className="row mb-3 align-items-center">
           <div className="col">
-            <label htmlFor="" className="mb-2">Password</label>
+            <label htmlFor="" className="mb-2 editLabel">Password <span className="text-danger">*</span></label>
             <input type="password" className="form-control" placeholder="*****" />
           </div>
           <div className="col-auto">
-            <button className="btn btn-primary my-4 editButton">Edit</button>
+            <button className="btn btn-primary my-4 editButton">Edit <span className="text-danger">*</span></button>
           </div>
         </div>
         <div className="row mb-3 align-items-center">
           <div className="col">
-            <label htmlFor="" className="mb-2">Address</label>
+            <label htmlFor="" className="mb-2 editLabel">Address <span className="text-danger">*</span></label>
             <textarea className="form-control" placeholder="Enter your address" id="floatingTextarea2"></textarea>
           </div>
           <div className="col-auto">
